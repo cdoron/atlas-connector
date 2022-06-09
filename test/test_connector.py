81bba97b13e7b9b9f46997f2d5e92f305f06d84e
Doron Chen <cdoron@il.ibm.com>
2022-06-09 20:05:45 +0300

    check update add asserts
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- test/test_connector.py
+++ test/test_connector.py
@@ -3,25 +3,42 @@
 import json
 import requests
 
-headers =  {"Content-Type":"application/json", "X-Request-Datacatalog-Write-Cred": "12345678"}
+headers =  {"Content-Type":"application/json"}
 
 with open("asset.json", "r") as f:
     data = f.read()
 
-data = json.loads(data)
-response = requests.post("http://localhost:8080/createAsset", json=data, headers=headers)
-print(response.text)
+asset_data = json.loads(data)
+
+print("Creating asset")
+response = requests.post("http://localhost:8080/createAsset", json=asset_data, headers=headers)
+
+assert response.status_code == 200
 
 assetID = response.json()["assetID"]
 print("Created Asset " + assetID)
 
 print("Now let us read that asset")
-
 data = {"assetID": assetID, "operationType": "read"}
 response = requests.post("http://localhost:8080/getAssetInfo", json=data, headers=headers)
-print(response.text)
+assert response.status_code == 200
 
+assert json.loads(response.text) == asset_data
+
+data = {"assetID": assetID, "name": "New Name"}
+response = requests.patch("http://localhost:8080/updateAsset", json=data, headers=headers)
+assert response.status_code == 200
+
+print("Now let us make sure that the asset has indeed changed")
+data = {"assetID": assetID, "operationType": "read"}
+response = requests.post("http://localhost:8080/getAssetInfo", json=data, headers=headers)
+assert response.status_code == 200
+
+assert json.loads(response.text) != asset_data
+asset_data["resourceMetadata"]["name"] = "New Name"
+assert json.loads(response.text) == asset_data
+
+print("Cleaning up -- deleting the asset")
 data = {"assetID": assetID}
 response = requests.delete("http://localhost:8080/deleteAsset", json=data, headers=headers)
-print(response.text)
-
+assert response.status_code == 200
